97af9f38daed79c997581aa92dbbe8442600eb46
Shawn Thompson <plansmash@me.com>
2018-12-10 20:43:23 -0500

    remove post from home page add class to h2 on homepage

--- index.php
+++ index.php
@@ -18,43 +18,8 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container">
 			<section id="about">
-			<h2>About a11yMTL</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed placeat vero aperiam optio, veritatis laudantium consectetur obcaecati, voluptas beatae temporibus ullam iusto odit est praesentium quae, quam deleniti similique dicta.</p>
-			</section>
-			<section>
-				<h2>Posts</h2>
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h3 class="page-title screen-reader-text"><?php single_post_title(); ?></h3>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+				<h2 class="text-center text-uppercase text-secondary mb-0">About a11yMTL</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed placeat vero aperiam optio, veritatis laudantium consectetur obcaecati, voluptas beatae temporibus ullam iusto odit est praesentium quae, quam deleniti similique dicta.</p>
 			</section>
 
 
@@ -104,7 +69,7 @@ endwhile;
 </section>
 
 			<section id="contact">
-				<h2>Contact us</h2>
+				<h2 class="text-center text-uppercase text-secondary mb-0">Contact us</h2>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt id assumenda unde, suscipit fuga perspiciatis commodi, sed necessitatibus quidem minima ipsam corrupti neque, facilis aspernatur non nobis perferendis numquam asperiores?</p>
 			</section>
 		</main><!-- #main -->
